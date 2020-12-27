207a239549798bf2e0c9f94193899ebf0632c758
davinder19 <48900931+davinder19@users.noreply.github.com>
2020-12-27 07:26:23 -0800

    local storage

--- Script.js
+++ Script.js
@@ -1,6 +1,10 @@
 const form = document.getElementById('form')
 const input = document.getElementById('input')
 const todosUL = document.getElementById('todos')
+const todos  = JSON.parse(localStorage.getItem('todos'))
+if(todos){
+    todos.forEach(todo => addTodo(todo))
+}
 
 form.addEventListener('submit', (e) =>{
     
@@ -21,16 +25,35 @@ if(todoText){
     }
     todoEl.innerText = todoText
 
-    todoEl.addEventListener('click',() => todoEl.classList.toggle('completed'))
+    todoEl.addEventListener('click',() =>{ 
+        todoEl.classList.toggle('completed')
+        updateLS()
+})
         
     todoEl.addEventListener('contextmenu',(e) =>{
              e.preventDefault()
 
              todoEl.remove()
+             updateLS()
     } )
 
     todosUL.appendChild(todoEl)
     input.value =''
+
+    updateLS()
 }
 
 }
+
+function updateLS(){
+    todoEl =document.querySelectorAll('li')
+    const todos = []
+    todoEl.forEach(todoEl =>{
+        todos.push({
+            text: todoEl.innerText,
+            completed: todoEl.classList.contains('completed')
+        })
+    })
+    localStorage.setItem('todos', JSON.stringify(todos))
+}
+// ************************save in Local Storage*************************
